ecf157df8e099df14716ebeeb297a13265d14368
Taylor <tay333@gmail.com>
2018-04-27 11:10:41 -0700

    yapf

--- py_bindings/setup.py
+++ py_bindings/setup.py
@@ -5,12 +5,7 @@ setup(
     name='gol_py',
     version='1.0',
     rust_extensions=[
-        RustExtension(
-            'gol_py._gol',
-            'Cargo.toml',
-            binding=Binding.PyO3
-        )
+        RustExtension('gol_py._gol', 'Cargo.toml', binding=Binding.PyO3)
     ],
     packages=['gol_py'],
-    zip_safe=False
-)
+    zip_safe=False)
