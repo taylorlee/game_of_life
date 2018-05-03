7be70ea9ca03bf48e4859a36604b00870bbcd530
Taylor <tay333@gmail.com>
2018-05-03 08:58:40 -0700

    close

--- py_bindings/bench.py
+++ py_bindings/bench.py
@@ -6,10 +6,10 @@ import os
 os.chdir('build/lib')
 import gol_py
 
-RPC_URL = 'https://localhost:3000/'
+RPC_URL = 'http://localhost:3000/'
 
 def rpc_setup():
     board = requests.get(RPC_URL+'setup/')
-    return set(json.loads(board.content))
+    return set(x for x in json.loads(board.content.decode()))
 
 
